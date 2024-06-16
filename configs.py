0bdea289c11df23591a47274dc3090d35fc9301c
PokemonOwner <160018170+PokemonOwner@users.noreply.github.com>
2024-06-16 15:04:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,5 +6,5 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "7150586146:AAGRVQq7GnAzdknNmUl2loEgy-ubaMOKm0k")
     FSUB = getenv("FSUB", "TechMon_UPSC")
     CHID = int(getenv("CHID", "-1002128064285"))
-    SUDO = list(map(int, getenv("SUDO").split(6815027331)))
+    SUDO = list(map(int, getenv("SUDO", "6815027331").split()))
     MONGO_URI = getenv("mongodb+srv://techmonofficial:techmon123@cluster0.wo0hzaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
